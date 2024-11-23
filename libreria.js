b8f86932809f25a84d43592e0b11b4994b6efb71
jonzta27 <jonsesjfarrieta2008@gmail.com>
2024-11-23 10:45:06 -0400

    tremendo cambio de el js

--- libreria.js
+++ libreria.js
@@ -1,1 +1,17 @@
-console.log("Holis mundo")
+const librosColeccion =[{
+    "Nombre": "Cien Años de Soledad",
+    "CantidadPaginas": 417,
+    "Autor": "Gabriel García Márquez",
+    "Genero": ["Realismo Mágico", "Ficción"],
+    "Disponible": true
+} ,
+{
+    "Nombre": "El Amor en los Tiempos del Cólera",
+    "CantidadPaginas": 348,
+    "Autor": "Gabriel García Márquez",
+    "Genero": ["Romance", "Ficción"],
+    "Disponible": false
+}
+];
+
+        function informacionlibro{}
